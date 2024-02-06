39bb571641534f23e9b94af418327472358d3675
MahogaHeryasa <hery.heryasa@gmail.com>
2024-02-06 23:20:48 +0700

    add product test

--- src/test/java/id/ac/ui/cs/advprog/eshop/model/ProductTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/ProductTest.java
@@ -5,26 +5,26 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 class ProductTest {
-     Product product;
-     @BeforeEach
-     void setup() {
+    Product product;
+    @BeforeEach
+    void setup() {
          this.product = new Product();
          this.product.setProductId("eb558e9f-1c39-460e-8860-71af6af63bd6");
          this.product.setProductName("Sampo Cap Bambang");
          this.product.setProductQuantity(100);
-     }
-     @Test
-     void testGetProductId() {
-         assertEquals("eb558e9f-1c39-460e-8860-71af6af63bd6", this.product.getProductId());
-     }
+    }
+    @Test
+    void testGetProductId() {
+        assertEquals("eb558e9f-1c39-460e-8860-71af6af63bd6", this.product.getProductId());
+    }
 
-     @Test
-     void testGetProductName() {
-         assertEquals( "Sampo Cap Bambang", this.product.getProductName());
-     }
+    @Test
+    void testGetProductName() {
+        assertEquals( "Sampo Cap Bambang", this.product.getProductName());
+    }
 
-     @Test
-     void testGetProductQuantity() {
-         assertEquals(100,this.product.getProductQuantity());
-     }
+    @Test
+    void testGetProductQuantity() {
+        assertEquals(100,this.product.getProductQuantity());
+    }
 }
