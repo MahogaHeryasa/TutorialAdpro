e31236651b69a07229c8257d84560955adc7a381
MahogaHeryasa <hery.heryasa@gmail.com>
2024-02-14 01:13:58 +0700

    fix build.gradle.kts for sonarcloud

--- build.gradle.kts
+++ build.gradle.kts
@@ -77,8 +77,8 @@ tasks.test {
 tasks.jacocoTestReport {
     dependsOn(tasks.test)
     reports {
-        html.required = true
         xml.required = true
+        html.required = true
     }
 }
 
